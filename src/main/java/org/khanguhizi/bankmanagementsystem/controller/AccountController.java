4be82255f9c23193a8243cefb712ffa3ac9c65d1
Cynthia Kanguhizi <Cynthia.Kanguhizi@ke.cicinsurancegroup.com>
2025-10-15 09:41:42 +0300

    Cross Origin Annotation

--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountController.java
@@ -6,10 +6,12 @@ import org.khanguhizi.bankmanagementsystem.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(originPatterns = "*", origins = {"*"})
 @RestController
 public class AccountController {
 @Autowired
