cd99ce8880b279866891e7444e468cb640dc5ca9
Cynthia Kanguhizi <Cynthia.Kanguhizi@ke.cicinsurancegroup.com>
2025-10-23 15:41:51 +0300

    CORS Error Solve

--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/CustomerController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(originPatterns = "*", origins = {"*"})
+@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
 @RestController
 public class CustomerController {
     @Autowired
