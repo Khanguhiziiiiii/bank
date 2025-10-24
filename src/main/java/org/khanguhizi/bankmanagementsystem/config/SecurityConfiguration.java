7fe9888915451ad8136429ebed32004eea7e162b
Cynthia Kanguhizi <Cynthia.Kanguhizi@ke.cicinsurancegroup.com>
2025-10-24 15:26:10 +0300

    JWT

---
+++ src/main/java/org/khanguhizi/bankmanagementsystem/config/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package org.khanguhizi.bankmanagementsystem.config;
+
+public class SecurityConfiguration {
+}
---
+++ src/main/java/org/khanguhizi/bankmanagementsystem/service/JWTService.java
@@ -0,0 +1,4 @@
+package org.khanguhizi.bankmanagementsystem.service;
+
+public class JWTService {
+}
