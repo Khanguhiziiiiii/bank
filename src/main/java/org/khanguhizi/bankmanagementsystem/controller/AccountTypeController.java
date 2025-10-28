195e717ca3e5e30097537aaa2c2e917336062d5f
Cynthia Kanguhizi <Cynthia.Kanguhizi@ke.cicinsurancegroup.com>
2025-10-28 15:11:49 +0300

    cors configuration

--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountTypeController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountTypeController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
+//@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
 @RestController
 public class AccountTypeController {
     @Autowired
--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/TransactionController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/TransactionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
+//@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
 @RestController
 public class TransactionController {
     @Autowired
