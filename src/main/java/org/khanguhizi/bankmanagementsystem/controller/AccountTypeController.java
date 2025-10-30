b461cc1e320c0c1c90a43b561d0319bae5be9726
Cynthia Kanguhizi <Cynthia.Kanguhizi@ke.cicinsurancegroup.com>
2025-10-30 09:35:14 +0300

    SwaggerUI

--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountTypeController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/AccountTypeController.java
@@ -1,7 +1,9 @@
 package org.khanguhizi.bankmanagementsystem.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.khanguhizi.bankmanagementsystem.dto.AccountTypeRequest;
-import org.khanguhizi.bankmanagementsystem.dto.ApiResponse;
 import org.khanguhizi.bankmanagementsystem.service.AccountTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,17 +12,28 @@ import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
 @RestController
+@Tag(name = "Account Types", description = "Endpoints for managing account types")
+@SecurityRequirement(name = "Bearer Token")
 public class AccountTypeController {
     @Autowired
     private AccountTypeService accountTypeService;
+
+    @Operation(
+            summary = "Creates a new account type",
+            description = "Creates account types from which the customers are supposed to choose from"
+    )
     @PostMapping ("/createAccountType")
-    public ResponseEntity <ApiResponse> createAccountType(@RequestBody AccountTypeRequest accountTypeRequest) {
+    public ResponseEntity <org.khanguhizi.bankmanagementsystem.dto.ApiResponse> createAccountType(@RequestBody AccountTypeRequest accountTypeRequest) {
         var createAccountTypeRes = accountTypeService.accountType(accountTypeRequest);
         return new ResponseEntity<>(createAccountTypeRes, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Fetches account types present in the database",
+            description = "Restricts account creation to only the account types present in the databased"
+    )
     @PostMapping("/fetchAccountType")
-    public ResponseEntity <ApiResponse> fetchAccountType(@RequestBody AccountTypeRequest accountTypeRequest) {
+    public ResponseEntity <org.khanguhizi.bankmanagementsystem.dto.ApiResponse> fetchAccountType(@RequestBody AccountTypeRequest accountTypeRequest) {
         var fetchAccountTypeRes = accountTypeService.fetchAccountType(accountTypeRequest);
         return new ResponseEntity<>(fetchAccountTypeRes, HttpStatus.OK);
     }
--- src/main/java/org/khanguhizi/bankmanagementsystem/controller/TransactionController.java
+++ src/main/java/org/khanguhizi/bankmanagementsystem/controller/TransactionController.java
@@ -1,5 +1,8 @@
 package org.khanguhizi.bankmanagementsystem.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.khanguhizi.bankmanagementsystem.dto.*;
 import org.khanguhizi.bankmanagementsystem.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +14,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@CrossOrigin(originPatterns = "*", origins = {"*"}, allowedHeaders = {"*"})
+
 @RestController
+@Tag(name = "Transactions", description = "Endpoints for managing transactions")
+@SecurityRequirement(name = "Bearer Token")
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
+
+    @Operation(
+            summary = "Facilitates deposit transactions",
+            description = "Accepts deposit amount, adds amount to current balance, displays updated balance"
+    )
     @PostMapping ("/deposit")
     public ResponseEntity<ApiResponse> deposit(@RequestBody TransactionRequest transactionRequest){
         var depositRes= transactionService.deposit(transactionRequest);
         return new ResponseEntity<>(depositRes, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Facilitates withdraw transactions",
+            description = "Accepts withdrawal amount, subtracts the amount from current balance, displays the updated balance"
+    )
     @PostMapping ("/withdraw")
-    @CrossOrigin("*")
     public ResponseEntity<ApiResponse> withdraw(@RequestBody TransactionRequest transactionRequest){
         var withdrawRes= transactionService.withdraw(transactionRequest);
         return new ResponseEntity<>(withdrawRes, HttpStatus.OK);
@@ -34,12 +48,20 @@ public class TransactionController {
         return new ResponseEntity<>(overdraftRes, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Facilitates check balance transactions",
+            description = "Allows user to see current balance"
+    )
     @PostMapping("/checkBalance")
     public ResponseEntity<ApiResponse> checkBalance(@RequestBody BalanceRequest balanceRequest){
         var checkBalanceRes= transactionService.checkBalance(balanceRequest);
         return new ResponseEntity<>(checkBalanceRes, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Facilitates transfer of funds between accounts",
+            description = "Allows user to input the "
+    )
     @PostMapping ("/transferFunds")
     public ResponseEntity<ApiResponse> transferFunds(@RequestBody TransferFundsRequest transferFundsRequest){
         var transferFundsRes= transactionService.transferFunds(transferFundsRequest);
